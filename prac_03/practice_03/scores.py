8400d089ad7166b2375924d7b9af6a0f7bc91510
David Renderos <david.renderos1@my.jcu.edu.au>
2020-08-17 12:40:59 +1000

    Add write functionality

--- prac_03/practice_03/scores.py
+++ prac_03/practice_03/scores.py
@@ -3,13 +3,21 @@ CP1404/CP5632 - Practical
 Complete program to determine score status
 """
 import random
+import csv
 
 
 def main():
+
+    out_file = open("results.txt", 'w')
+    out_file_writer = csv.writer(out_file)
+
     number_of_scores = int(input("Enter number on scores: "))
-    for i in range(1, number_of_scores + 1):
+    for row in range(1, number_of_scores + 1):
         random_score = (random.randint(1, 100))
-        print("{} is {}".format(random_score, result(random_score)))
+        # print("{} is {}".format(random_score, result(random_score), file=out_file))
+        # print("{} is {}".format(random_score, result(random_score)))
+        out_file_writer.writerow("{} is {}".format(random_score, result(random_score)).split(', '))
+    out_file.close()
 
 
 def result(score):
