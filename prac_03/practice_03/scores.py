558d155bd02abc180d3ec1ecdedd4ce657f2d7c8
David Renderos <david.renderos1@my.jcu.edu.au>
2020-08-17 12:57:50 +1000

    Remove old code comment

--- prac_03/practice_03/scores.py
+++ prac_03/practice_03/scores.py
@@ -14,8 +14,6 @@ def main():
     number_of_scores = int(input("Enter number on scores: "))
     for row in range(1, number_of_scores + 1):
         random_score = (random.randint(1, 100))
-        # print("{} is {}".format(random_score, result(random_score), file=out_file))
-        # print("{} is {}".format(random_score, result(random_score)))
         out_file_writer.writerow("{} is {}".format(random_score, result(random_score)).split(', '))
     out_file.close()
 
