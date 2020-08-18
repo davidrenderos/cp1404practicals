1203c7d29785ca072677f1df253b2414c77a3e87
David Renderos <david.renderos1@my.jcu.edu.au>
2020-08-18 12:52:08 +1000

    Add while loop for any number of numbers input

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,16 +1,19 @@
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 numbers = []
-
-for count in range(5):
-    number = int(input("Number: "))
+count = 1
+number = int(input("Number {}: ".format(count)))
+numbers.append(number)
+while number > 0:
+    count += 1
+    number = int(input("Number {}: ".format(count)))
     numbers.append(number)
-
-print("The first number is {}".format(numbers[0]))
-print("The last number is {}".format(numbers[-1]))
-print("The smallest number is {}".format(min(numbers)))
-print("The largest number is {}".format(max(numbers)))
-print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+else:
+    print("The first number is {}".format(numbers[0]))
+    print("The last number is {}".format(numbers[-1]))
+    print("The smallest number is {}".format(min(numbers)))
+    print("The largest number is {}".format(max(numbers)))
+    print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
 
 username = input("Enter username: ")
 if username in usernames:
