124ce3086650a79c88453fef2be11163773c7152
David Renderos <david.renderos1@my.jcu.edu.au>
2020-08-18 11:11:55 +1000

    Improve functionality of quick picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,17 +1,18 @@
 import random
 
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
 
 def main():
-    list_of_numbers = []
-
     number_of_quick_picks = int(input("How many quick picks? "))
-
     for i in range(number_of_quick_picks):
-        sub_list_of_numbers = []
-        for j in range(5):
-            sub_list_of_numbers.append(random.randint(1, 45))
-        list_of_numbers.append(sub_list_of_numbers)
-    print(sorted(list_of_numbers))
+        list_of_numbers = []
+        for j in range(NUMBERS_PER_LINE):
+            number = (random.randint(MINIMUM, MAXIMUM))
+            list_of_numbers.append(number)
+        print(list_of_numbers)
 
 
 main()
