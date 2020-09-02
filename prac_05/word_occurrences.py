c0f71b0b4601fdd2cab134ea13ef455dd11d9415
David Renderos <david.renderos1@my.jcu.edu.au>
2020-09-02 16:00:14 +1000

    Add sort capability

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,7 +5,9 @@ for word in text_list:
     count = word_count_dict.get(word, 0)
     word_count_dict[word] = count + 1
 
-
+# Add sort capability
 text_list = list(word_count_dict.keys())
+text_list.sort()
+
 for word in text_list:
     print("{} : {}".format(word, word_count_dict[word]))
