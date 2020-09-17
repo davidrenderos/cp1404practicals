ae859efd2a8cd828a606af835faa237ae0325713
David Renderos <david.renderos1@my.jcu.edu.au>
2020-09-17 17:13:30 +1000

    Add languages and programing languages programs

---
+++ prac_06/languages.py
@@ -0,0 +1,15 @@
+from prac_06 import programming_language
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(ruby)
+    print(python)
+    print(visual_basic)
+
+
+main()
