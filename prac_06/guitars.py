48739233955d6fe9a407a092c96544046eef24af
David Renderos <david.renderos1@my.jcu.edu.au>
2020-09-17 19:26:31 +1000

    Refactor name

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,18 +1,27 @@
-CURRENT_YEAR = 2020
-VINTAGE_AGE = 50
+from prac_06.guitar import Guitar
 
-class Guitar:
 
-    def __init__(self, name="", year=0, cost=0):
-        self.name = name
-        self.year = year
-        self.cost = cost
+def main():
+    guitars = []
+    print("My guitars!")
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = int(input("Cost: "))
+        guitars_to_add = Guitar(name, year, cost)
+        guitars.append(guitars_to_add)
+        print(guitars_to_add, "added.")
+        name = input("Name: ")
+    guitars.sort()
+    print("These are my Guitars!")
+    for i, guitar in enumerate(guitars):
+        vintage_string = ""
+        if guitar.is_vintage():
+            vintage_string = "(vintage)"
+        print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".
+              format(i + 1, guitar, vintage_string))
 
-    def __str__(self):
-        return"{}, {}, {}".format(self.name, self.year, self.cost)
 
-    def get_age(self):
-        return CURRENT_YEAR - self.year
-
-    def is_vintage(self):
-        return self.get_age() >= VINTAGE_AGE
+main()
